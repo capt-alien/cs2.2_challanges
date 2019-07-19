df4fc10b6c26d62bd23f624d33cfe1ee62716ead
capt-alien <eric.botcher@presidio.edu>
2019-07-18 18:46:23 -0700

    added Graphy text file converter to adjanacy matrix

--- adjancy_matrix.py
+++ adjancy_matrix.py
@@ -1,5 +1,7 @@
 #!python3
 # Code Inspired by Ansel's work.
+import sys
+from ast import literal_eval as make_tuple
 
 class Graph(object):
     """Impliments a graph as a list
@@ -9,6 +11,7 @@ class Graph(object):
         self.name_to_index = {}
         self.index_to_name = {}
         self.graph = []
+        self.edge_list = []
         # impliment self.size later
 
     def __repr__(self):
@@ -51,6 +54,8 @@ class Graph(object):
         if self.graph[vert_1_index][vert_2_index] != 1:
             self.graph[vert_1_index][vert_2_index] = -1
 
+        self.edge_list.append((vert_1, vert_2))
+
     def add_weighted_edge(self, vert_1, vert_2, weight):
         """Adds a weighted edge between two
             verticies"""
@@ -81,3 +86,33 @@ class Graph(object):
                 neighbors.add(self.index_to_name[index])
 
         return neighbors
+
+
+def graphify_text_file(filename):
+    f=open(filename, "r", encoding="utf8")
+    lines = f.readlines()
+    f.close()
+    #create Graph
+    graph = Graph()
+    #get rid of \n
+    lines = list(map(lambda line: line.strip('\n'), lines))
+    for x in range(2, len(lines)):
+        lines[x] = make_tuple(lines[x])
+    #Create Verticies in line
+    vertices = make_tuple(lines[1])#.split(',')
+    for x in vertices:
+        graph.add_vertex(x)
+    #Run the rest of the list through a for loop
+    for y in range(2, len(lines)):
+        #check len for weighted
+        graph.add_edge(lines[y][0], lines[y][1])
+
+    return graph
+
+if __name__ == '__main__':
+    grapher = graphify_text_file(sys.argv[1])
+    print("Number of Verticies:"+str(grapher.size))
+    print("Number of Edges:"+str(len(grapher.edge_list)))
+    print("Edge list:")
+    for edge in grapher.edge_list:
+        print(edge)

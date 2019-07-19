cae5c6b62feafc455ac80eb22477e1c3353860c7
capt-alien <eric.botcher@presidio.edu>
2019-07-18 18:56:49 -0700

    cleaned up a bit

--- challange_1/adjancy_list.py
+++ challange_1/adjancy_list.py
@@ -3,57 +3,44 @@
 import sys
 from ast import literal_eval as make_tuple
 
+
 class Graph(object):
     """Impliments a graph as a list
         of Adjancies"""
 
     def __init__(self):
-        self.name_to_index = {}
-        self.index_to_name = {}
-        self.graph = []
+        self.graph = {}
+        self.size = 0
         self.edge_list = []
 
     def __repr__(self):
         return "graph({})".format(self.graph)
 
-    @property
     def size(self):
-        return len(self.graph)
+        return self.size
 
     def add_vertex(self, vert):
         """Add a Vertex to the Graph."""
         #check to see if vertex in graph
-        if vert in self.name_to_index:
+        if vert in self.graph:
             raise KeyError("{} is already in the Graph".format(vert))
-
-        self.name_to_index[vert] = self.size
-        self.index_to_name[self.size] = vert
-
-        for row in self.graph:
-            row.append(0)
-
-        # Because vertex has not been added yet, range should account for
-        # missing vertex with a +1
-        self.graph.append([0 for vertex in range(self.size + 1)])
+        self.graph[vert]= set()
+        self.size += 1
 
     def add_edge(self, vert_1, vert_2):
         """Adds a direct edge between two
             verticies"""
-        if vert_1 not in self.name_to_index:
+        if vert_1 not in self.graph:
             self.add_vertex(vert_1)
 
-        if vert_2 not in self.name_to_index:
+        if vert_2 not in self.graph:
             self.add_vertex(vert_2)
 
-        vert_1_index = self.name_to_index[vert_1]
-        vert_2_index = self.name_to_index[vert_2]
-
-        self.graph[vert_1_index][vert_2_index] = 1
-
-        if self.graph[vert_1_index][vert_2_index] != 1:
-            self.graph[vert_1_index][vert_2_index] = -1
+        self.graph[vert_1].add(vert_2)
+        self.graph[vert_2].add(vert_1)
 
         self.edge_list.append((vert_1, vert_2))
+
 
     def add_weighted_edge(self, vert_1, vert_2, weight):
         """Adds a weighted edge between two
@@ -63,28 +50,21 @@ class Graph(object):
     def has_vertex(self, vert_key):
         """Find the vertex in the graph named vert_key
             and if it exists returns True."""
-        if vert_key not in self.name_to_index:
+        if vert_key not in self.graph:
             return False
         return True
 
     def get_vertices(self):
         """Retunrn the list of all verticies in the graph."""
-        return [vertex for vertex in self.name_to_index.keys()]
+        verticies = [vertex for vertex in self.graph.keys()]
+        return verticies
 
     def get_neighbors(self, vert):
         """Lasts all vertices y such that there is an
             edge from the vert to the vertex y"""
-        if vert not in self.name_to_index:
-            raise KeyError("{} vertex is not in graph".format(vert))
-
-        vert_index = self.name_to_index[vert]
-        neighbors = set()
-
-        for index, edge in enumerate(self.graph[vert_index]):
-            if edge == 1:
-                neighbors.add(self.index_to_name[index])
-
-        return neighbors
+        if vert not in self.graph:
+            raise KeyError("Vertex {} is not in the graph")
+        return self.graph[vert]
 
 
 def graphify_text_file(filename):
@@ -105,8 +85,9 @@ def graphify_text_file(filename):
     for y in range(2, len(lines)):
         #check len for weighted
         graph.add_edge(lines[y][0], lines[y][1])
-
+        
     return graph
+
 
 if __name__ == '__main__':
     grapher = graphify_text_file(sys.argv[1])

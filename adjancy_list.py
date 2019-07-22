907788b481ac7614d9b7e37bc845df3e5b170455
capt-alien <eric.botcher@presidio.edu>
2019-07-22 13:24:19 -0700

    added weights and edge list

--- adjancy_list.py
+++ adjancy_list.py
@@ -1,8 +1,5 @@
 #!python3
 # Code Inspired by Ansel's work.
-import sys
-from ast import literal_eval as make_tuple
-
 
 class Graph(object):
     """Impliments a graph as a list
@@ -10,14 +7,17 @@ class Graph(object):
 
     def __init__(self):
         self.graph = {}
-        self.size = 0
-        self.edge_list = []
+        self.weights = {}
+        self.weighted = weighted
+        self.directed = directed
+        self.edges = 0
 
     def __repr__(self):
         return "graph({})".format(self.graph)
 
+    @property
     def size(self):
-        return self.size
+        return len(self.graph)
 
     def add_vertex(self, vert):
         """Add a Vertex to the Graph."""
@@ -32,20 +32,34 @@ class Graph(object):
             verticies"""
         if vert_1 not in self.graph:
             self.add_vertex(vert_1)
-
         if vert_2 not in self.graph:
             self.add_vertex(vert_2)
 
         self.graph[vert_1].add(vert_2)
-        self.graph[vert_2].add(vert_1)
+        # self.graph[vert_2].add(vert_1)
+        self.edges += 1
 
-        self.edge_list.append((vert_1, vert_2))
 
-
-    def add_weighted_edge(self, vert_1, vert_2, weight):
+    def add_weighted_edge(self, vert_1, vert_2, weight=0):
         """Adds a weighted edge between two
-            verticies"""
-        pass
+            verticies. If no weight given it defaults to a
+             weight of 0"""
+        assert isinstance(weight, int), "weight is not an intiger"
+
+        if vert_1 not in self.graph:
+            self.add_vertex(vert_1)
+        if vert_2 not in self.graph:
+            self.add_vertex(vert_2)
+
+        if vert_2 not in self.graph[vert_1]:
+            slef.graph[vert_1].add(vert_2)
+            # if the graph is weighted, store edge weights in dictionary
+            self.weights[(vert_1, vert_2)] = weight
+
+        # If the graph can go both ways, add edge going opposite way
+        if self.directed is False:
+            self.graph[vert_2].add(vert_1)
+            self.weights[(vert_2, vert_1)] = weight
 
     def has_vertex(self, vert_key):
         """Find the vertex in the graph named vert_key
@@ -66,33 +80,28 @@ class Graph(object):
             raise KeyError("Vertex {} is not in the graph")
         return self.graph[vert]
 
+        def get_edges(self):
+            """Returns the list of all edges and weights in the graph."""
+            edge_list = set()
 
-def graphify_text_file(filename):
-    f=open(filename, "r", encoding="utf8")
-    lines = f.readlines()
-    f.close()
-    #create Graph
-    graph = Graph()
-    #get rid of \n
-    lines = list(map(lambda line: line.strip('\n'), lines))
-    for x in range(2, len(lines)):
-        lines[x] = make_tuple(lines[x])
-    #Create Verticies in line
-    vertices = make_tuple(lines[1])#.split(',')
-    for x in vertices:
-        graph.add_vertex(x)
-    #Run the rest of the list through a for loop
-    for y in range(2, len(lines)):
-        #check len for weighted
-        graph.add_edge(lines[y][0], lines[y][1])
-        
-    return graph
+            for vert_1 in self.graph:
+                for vert_2 in self.get_neighbors(vert_1):
+                    if self.weighted:
+                        weight = self.weights[(vert_1, vert_2)]
 
+                      # If the graph is directed, as to edge list as normal
+                    if self.directed and self.weighted:
+                        edge_list.add((vert_1, vert_2, weight))
+                    if self.directed and not self.weighted:
+                        edge_list.add((vert_1, vert_2))
 
-if __name__ == '__main__':
-    grapher = graphify_text_file(sys.argv[1])
-    print("Number of Verticies:"+str(grapher.size))
-    print("Number of Edges:"+str(len(grapher.edge_list)))
-    print("Edge list:")
-    for edge in grapher.edge_list:
-        print(edge)
+                                # If the graph is undirected, make sure only one edge between
+                # two vertices is counted. My implementation stores a directed
+                # edge from and to both vertices for easier traversals.
+                    if not self.directed and self.weighted:
+                        if (vert_1, vert_2, weight) not in edge_list:
+                            edge_list.add((fvert_1, vert_2, weight))
+                    if not self.directed and not self.weighted:
+                        if (vert_1, vert_2) not in edge_list:
+                            edge_list.add((vert_1, vert_2))
+        return edge_list
---
+++ challenge_1.py
@@ -0,0 +1,41 @@
+#!python3
+import sys
+from ast import literal_eval as make_tuple
+
+from adjancy_list import Graph
+# from adjancy_matrix import Graph
+
+def graphify_text_file(filename):
+    f=open(filename, "r", encoding="utf8")
+    lines = f.readlines()
+    f.close()
+    #create Graph
+    graph = Graph()
+    #get rid of \n
+    lines = list(map(lambda line: line.strip('\n'), lines))
+    for x in range(2, len(lines)):
+        lines[x] = make_tuple(lines[x])
+    #Create Verticies in line
+    vertices = make_tuple(lines[1])#.split(',')
+    for x in vertices:
+        graph.add_vertex(x)
+    #Run the rest of the list through a for loop
+    for y in range(2, len(lines)):
+        #check len for weighted
+        graph.add_edge(lines[y][0], lines[y][1])
+
+    return graph
+
+def print_graph(graph):
+    print("Number of Verticies:"+str(graph.size))
+    print("Number of Edges:"+str(graph.edges))
+    print("Edge list:")
+    #itterate through vertexes and get edges
+    edges = graph.get_edges()
+    for edge in edges:
+        print(edge)
+
+
+if __name__ == '__main__':
+    grapher = graphify_text_file(sys.argv[1])
+    print_graph(grapher)
